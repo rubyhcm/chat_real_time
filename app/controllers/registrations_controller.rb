7ec3ebb051ab73044986a9df653451aac9ebf46b
nguyenloi <loindp@ipinc.io>
2023-10-09 00:50:33 +0700

    2 - Optimize controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,19 +5,24 @@ class RegistrationsController < ApplicationController
     @user = User.new
   end
 
-  def create 
+  def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      sign_in(user)
       redirect_to chatrooms_path
     else
-      redirect_to signup_path, flash[:notice] =  user.errors.messages
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to signup_path
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:username)
-    end
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,38 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, only: [:destroy]
-  
+
   def new
     @user = User.new
   end
 
   def create
-    user = User.find_by(user_params)
+    user = User.find_by(username: user_params[:username])
     if user
-      session[:user_id] = user.id
+      sign_in(user)
       redirect_to chatrooms_path
     else
-      redirect_to login_path, flash[:notice] =  {username: ["doesn't exist"]}
+      flash[:error] = "Username doesn't exist"
+      redirect_to login_path
     end
   end
 
   def destroy
-    reset_session
+    sign_out
     redirect_to root_path
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:username)
-    end
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def sign_out
+    reset_session
+  end
 end
